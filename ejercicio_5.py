ee6f8457d25e4faa1bc17bbdb96e55c73bcb79de
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2025-03-12 07:42:58 -0500

    Actualización

---
+++ ejercicio_5.py
@@ -0,0 +1,32 @@
+import random
+
+#Definir función
+
+def calcular_promedio_pares(lista):
+    suma = 0
+    contador_pares = 0
+    for i in lista:
+        if i % 2 == 0:
+            contador_pares += 1
+            suma = suma + i
+    promedio = suma / contador_pares
+    return promedio
+
+
+#input
+#creamos lista vacia
+
+lista = []
+
+#Tamaño de la lista
+
+n = int(input("Digite el tamaño de la lista: "))
+
+for i in range(n):
+    num = random.randint(1,n)
+    lista.append(num)
+
+#processing
+
+print(f"lista: {lista}")
+print(f"el promedio de la lista es {calcular_promedio_pares(lista)}")
