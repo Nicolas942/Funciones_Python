02528c5dd1f58db81a2a43ac02b6cebd25a6fa1e
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2025-03-12 06:52:21 -0500

    Actualización

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -1,13 +1,19 @@
 import random
 
+#Definición de la función
+
 def Sumalista(lista):
     suma = 0
     for i in lista:
         suma = suma + i
     return suma
     
+#input
+#creamos lista vacia
 
 lista = []
+
+#Tamaño de la lista
 
 n = int(input("Dijite el tamaño de la lista: "))
 
@@ -15,6 +21,8 @@ for i in range(n):
     num = random.randint(1,9)
     lista.append(num)
 
+
+#processing
 
 print(f"lista: {lista}")
 print(f"la suma es {Sumalista(lista)}")
